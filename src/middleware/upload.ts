29d0df5071c5e4e81c7079be1e71d5db6deaff70
shail-wo <shailesh.webosmotic@gmail.com>
2025-07-30 18:49:39 +0530

    feat(upload): add axios dependency and webhook for file uploads
    - Add axios package for HTTP requests
    - Implement webhook to notify another service after S3 upload
    - Simplify filename generation using UUID instead of cleaning original name
    - Remove unused common functions and clean up code

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { cleanFilename, uploadFile } from "../config/bucket/service";
 import * as path from "path";
+import { generateUUID } from "./../common/common-function";
+import axios from "axios";
 
 export const uploadFileS3 = async (
   req: Request,
@@ -15,25 +17,37 @@ export const uploadFileS3 = async (
     console.log("Final req.params----->", req.params);
 
     const { companyId, type } = req.params;
+    const { cabinetId } = req.query;
     console.log("companyId----->", companyId);
     console.log("type----->", type);
+    console.log("cabinetId----->", cabinetId);
 
     const originalName = req.file.originalname;
     const ext = path.extname(originalName); // includes leading dot, e.g. ".pdf"
-    const baseName = path.parse(originalName).name; // filename without extension
 
-    const cleanedBase = await cleanFilename(baseName);
-
+    console.log("ext----->", ext);
     // build unique S3 key
     const timestamp = Date.now();
-    const folder = `${companyId}/${type}`; // e.g. "COMPANYID/evidence"
-    const filename = `${timestamp}-${cleanedBase}${ext}`;
+    const folder = `${type}`; // e.g. "evidence"
+    const filename = `${timestamp}-${generateUUID()}`;
     const fullKey = `${folder}/${filename}`;
 
     // upload buffer
     const s3Key = await uploadFile(fullKey, req.file.buffer, req.file.mimetype);
 
     console.log("s3Key----->", s3Key);
+
+    try {
+      // Send the S3 key to another server
+      await axios.post("http://localhost:3000/web-hook/set-s3-filepath", {
+        location: s3Key, // 👈 sending location in body
+        companyId,
+        type,
+        cabinetId,
+      });
+    } catch (error: any) {
+      console.log("error in axios post----->", error);
+    }
 
     // respond or attach to req for further handling
     res.json({ location: s3Key });
